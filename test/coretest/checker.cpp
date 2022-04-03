e3b17935f54e3ae0614fb522dc23b2bbfc604b62
selia1078 <871353109@qq.com>
2022-04-03 21:39:26 +0800

    AutoUnitTest

--- test/coretest/checker.cpp
+++ test/coretest/checker.cpp
@@ -85,6 +85,7 @@ namespace autochecker
                 (*len) = wordCount;
             }
             fclose(file);
+
             return 0;
         }
 
@@ -190,33 +191,13 @@ namespace autochecker
         }
 
         int checker(char START, char END, bool find_all, bool tot_character, bool first_diff) {
-            /*FILE* file;
-            int r = fopen_s(&file, filename, "r");
-            std::string s = "";
-            wordSet.clear();
+            ansLen = 0;
 
-            char c;
-            while ((c = fgetc(file)) != EOF) {
-                if (c >= 'A' && c <= 'Z')
-                    s += char(c - 'A' + 'a');
-                else if (c >= 'a' && c <= 'z')
-                    s += c;
-                else {
-                    if ((int)s.size() > 1) {
-                        if (wordSet.find(s) == wordSet.end()) {
-                            word[++totWord] = s;
-                        }
-                    }
-                    s = "";
-                }
+            te = 0;
+            for (int i = 0; i < 26; i++) {
+                first[i] = 0;
             }
 
-            fclose(file);
-
-            if ((int)s.size() > 1 && wordSet.find(s) == wordSet.end()) {
-                word[++totWord] = s;
-            }
-            */
             for (int i = 1; i <= totWord; i++) {
                 link(word[i][0] - 'a', word[i][word[i].size() - 1] - 'a', i, tot_character);
             }
@@ -255,33 +236,614 @@ namespace autochecker
             
 
             int std_ans = checker(head, tail, false, false, false);
+            if (std_ans == 1)
+                std_ans = 0;
 
             char path[100] = "../test/output.txt";
-            //output(path, len, words, len);
 
             int ans = gen_chain_word(words, len, result, head, tail, true);
 
-            output(path, ans, result, (ans > 0));
-            //Assert::AreEqual(ans, std_ans);
+           // output(path, ans, result, (ans > 0));
+            Assert::AreEqual(ans, std_ans);
             //Assert::AreEqual(ans, std_ans);
         }
         
         TEST_METHOD(autotest0)
         {
 
+            char filename[100] = "../test/input.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 'e', 'f', 0
+            };
+            char mtail[10] = {
+                'a', 'b', 'c', 'd', 'e', 'f', 0
+            };
+
+            for (int i = 0; i < 7; i++) {
+                char head = mhead[i];
+;                for (int j = 0; j < 7; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest1)
+        {
+
+            char filename[100] = "../test/input1.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'w', 'o', 'm', 'n', 0
+            };
+
+            for (int i = 0; i < 5; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 5; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+
+        TEST_METHOD(autotest2)
+        {
+
+            char filename[100] = "../test/input2.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest3)
+        {
+
+            char filename[100] = "../test/input3.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[11] = {
+                'a', 'z', 'e', 'u', 'f',
+                'd', 'm', 'l', 't', 'p',
+                0
+            };
+
+            for (int i = 0; i < 11; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 11; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest4)
+        {
+
             char filename[100] = "../test/input4.txt";
             int len = 0;
             int r = handleInput(filename, words, &len);
-            for (int i = 0; i < 10; i++) {
-                char head = 'a' + i;
-;                for (int j = 0; j < 10; j++) {
-                    char tail = 'a' + j;
-                    
+
+            char mhead[10] = {
+                'e', 'h', 't', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest5)
+        {
+
+            char filename[100] = "../test/input5.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest6)
+        {
+
+            char filename[100] = "../test/input6.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 'e', 0
+            };
+
+            for (int i = 0; i < 6; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 6; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest7)
+        {
+
+            char filename[100] = "../test/input7.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest8)
+        {
+
+            char filename[100] = "../test/input8.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
                     gen_word_fft(head, tail, len);
                 }
             }
         }
         
+        TEST_METHOD(autotest9)
+        {
+
+            char filename[100] = "../test/input9.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest10)
+        {
+
+            char filename[100] = "../test/input10.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 0
+            };
+
+            for (int i = 0; i < 5; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 5; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest11)
+        {
+
+            char filename[100] = "../test/input11.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'c', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest12)
+        {
+
+            char filename[100] = "../test/input12.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'f',
+                'o', 'h', 'u', 'y', 0
+            };
+
+            for (int i = 0; i < 9; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 9; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotest13)
+        {
+
+            char filename[100] = "../test/input13.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'c', 'd', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_word_fft(head, tail, len);
+                }
+            }
+        }
+
+        void gen_char_fft(char head, char tail, int len) {
+
+
+            int std_ans = checker(head, tail, false, true, false);
+            if (std_ans == 1)
+                std_ans = 0;
+
+            char path[100] = "../test/output.txt";
+
+            int ans = gen_chain_word(words, len, result, head, tail, true);
+
+            int ans_len = 0;
+            for (int i = 0; i < strlen(result[1]); i++) {
+                if (result[1][i] >= 'a' && result[1][i] <= 'z')
+                    ans_len ++;
+            }
+
+            output(path, ans, result, (ans > 0));
+            Assert::AreEqual(ans_len, std_ans);
+            //Assert::AreEqual(ans, std_ans);
+        }
+
+        TEST_METHOD(autotestchar0)
+        {
+
+            char filename[100] = "../test/input.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 'e', 'f', 0
+            };
+            char mtail[10] = {
+                'a', 'b', 'c', 'd', 'e', 'f', 0
+            };
+
+            for (int i = 0; i < 1; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 1; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar1)
+        {
+
+            char filename[100] = "../test/input1.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'w', 'o', 'm', 'n', 0
+            };
+
+            for (int i = 0; i < 5; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 5; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+
+        TEST_METHOD(autotestchar2)
+        {
+
+            char filename[100] = "../test/input2.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar3)
+        {
+
+            char filename[100] = "../test/input3.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[11] = {
+                'a', 'z', 'e', 'u', 'f',
+                'd', 'm', 'l', 't', 'p',
+                0
+            };
+
+            for (int i = 0; i < 11; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 11; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar4)
+        {
+
+            char filename[100] = "../test/input4.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'e', 'h', 't', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar5)
+        {
+
+            char filename[100] = "../test/input5.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar6)
+        {
+
+            char filename[100] = "../test/input6.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 'e', 0
+            };
+
+            for (int i = 0; i < 6; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 6; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar7)
+        {
+
+            char filename[100] = "../test/input7.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar8)
+        {
+
+            char filename[100] = "../test/input8.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar9)
+        {
+
+            char filename[100] = "../test/input9.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar10)
+        {
+
+            char filename[100] = "../test/input10.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'd', 0
+            };
+
+            for (int i = 0; i < 5; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 5; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar11)
+        {
+
+            char filename[100] = "../test/input11.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'c', 0
+            };
+
+            for (int i = 0; i < 3; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 3; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar12)
+        {
+
+            char filename[100] = "../test/input12.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'b', 'c', 'f',
+                'o', 'h', 'u', 'y', 0
+            };
+
+            for (int i = 0; i < 9; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 9; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
+        TEST_METHOD(autotestchar13)
+        {
+
+            char filename[100] = "../test/input13.txt";
+            int len = 0;
+            int r = handleInput(filename, words, &len);
+
+            char mhead[10] = {
+                'a', 'c', 'd', 0
+            };
+
+            for (int i = 0; i < 4; i++) {
+                char head = mhead[i];
+                for (int j = 0; j < 4; j++) {
+                    char tail = mhead[j];
+                    gen_char_fft(head, tail, len);
+                }
+            }
+        }
+
 	};
 }
 
